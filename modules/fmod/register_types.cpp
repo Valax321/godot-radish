e3bc4c3d38f06a71e96f0ffea453c48d130a3f66
Audrey Castillo <valax321@gmail.com>
2025-10-22 19:14:24 +1100

    Scaffolding for fmod module
    Still need to add windows libs from windows PC. Why do they ship an installer???

---
+++ modules/fmod/register_types.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Radish Games. All rights reserved.
+
+#include "register_types.h"
+
+#include "core/object/class_db.h"
+
+void initialize_fmod_module(ModuleInitializationLevel p_level) {
+
+}
+
+void uninitialize_fmod_module(ModuleInitializationLevel p_level) {
+
+}
---
+++ modules/fmod/register_types.h
@@ -0,0 +1,8 @@
+// Copyright (c) Radish Games. All rights reserved.
+
+#pragma once
+
+#include "modules/register_module_types.h"
+
+void initialize_fmod_module(ModuleInitializationLevel p_level);
+void uninitialize_fmod_module(ModuleInitializationLevel p_level);
